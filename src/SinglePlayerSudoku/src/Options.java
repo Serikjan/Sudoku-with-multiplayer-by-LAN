e33021f541177782de3b25b93a947e43801af493
Batkhuu <bybatkhuu@yahoo.com>
2012-05-31 13:10:22 +0900

    Inserted Options from's GUI.

--- src/SinglePlayerSudoku/src/Options.java
+++ src/SinglePlayerSudoku/src/Options.java
@@ -7,16 +7,20 @@ import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 
 @SuppressWarnings("serial")
 public class Options extends JFrame implements ActionListener
 {
 	private JLabel labelTime;
+	private JLabel labelMin;
+	private JLabel labelSec;
 	private ButtonGroup groupDiff;
 	private JRadioButton radioEasy;
 	private JRadioButton radioMedium;
@@ -27,18 +31,22 @@ public class Options extends JFrame implements ActionListener
 	private JCheckBox checkSolver;
 	private JCheckBox checkPause;
 	private JCheckBox checkTime;
+	private JComboBox comboSolveN;
+	private JComboBox comboPauseN;
+	private JTextField textMin;
+	private JTextField textSec;
 	private JButton buttonOk;
 	private JButton buttonCancel;
 	
 	Options()
 	{
 		this.setLayout(null);
-		this.setBounds(500, 200, 400, 400);
+		this.setBounds(500, 200, 320, 250);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("Options");
 		
-		radioEasy = new JRadioButton("Easy",true);
+		radioEasy = new JRadioButton("Easy", true);
 		radioEasy.setName("Easy");
 		radioEasy.addActionListener(this);
 		radioMedium = new JRadioButton("Medium");
@@ -58,39 +66,93 @@ public class Options extends JFrame implements ActionListener
 		panelDiff.add(radioEasy);
 		panelDiff.add(radioMedium);
 		panelDiff.add(radioHard);
-		panelDiff.setBounds(10, 10, 100, 100);
+		panelDiff.setBounds(15, 10, 100, 100);
 		panelDiff.setBorder(BorderFactory.createTitledBorder("Difficulty:"));
-		
 		
 		checkNode = new JCheckBox("Use node checker");
 		checkNode.setName("NodeChecker");
-		checkNode.setBounds(140, 10, 200, 25);
+		checkNode.setSelected(true);
+		checkNode.setBounds(160, 10, 200, 25);
 		checkNode.addActionListener(this);
-		//checkNode.setBorder(BorderFactory.createLineBorder(Color.black));
+		
 		checkPoint = new JCheckBox("Show points");
 		checkPoint.setName("ShowPoint");
-		checkPoint.setBounds(140, 45, 150, 25);
+		checkPoint.setSelected(true);
+		checkPoint.setBounds(160, 45, 150, 25);
 		checkPoint.addActionListener(this);
+		
 		checkSolver = new JCheckBox("Use solve");
 		checkSolver.setName("UseSolver");
-		checkSolver.setBounds(140, 70, 150, 25);
+		checkSolver.setSelected(true);
+		checkSolver.setBounds(160, 80, 100, 25);
 		checkSolver.addActionListener(this);
+		
+		comboSolveN = new JComboBox();
+		for(int i=1; i<10; i++)
+		{
+			comboSolveN.addItem(i);
+		}
+		comboSolveN.setBounds(260, 80, 40, 25);
+		comboSolveN.addActionListener(this);
+		
 		checkPause = new JCheckBox("Use pause");
 		checkPause.setName("UsePause");
-		checkPause.setBounds(140, 105, 150, 25);
+		checkPause.setSelected(true);
+		checkPause.setBounds(160, 115, 100, 25);
 		checkPause.addActionListener(this);
+		
+		comboPauseN = new JComboBox();
+		for(int i=1; i<10; i++)
+		{
+			comboPauseN.addItem(i);
+		}
+		comboPauseN.setBounds(260, 115, 40, 25);
+		comboPauseN.addActionListener(this);
 		
 		checkTime = new JCheckBox("Time default (10 min)");
 		checkTime.setName("Set Time");
-		checkTime.setBounds(10, 110, 100, 25);
+		checkTime.setSelected(true);
+		checkTime.setBounds(10, 115, 150, 25);
 		checkTime.addActionListener(this);
+		
+		labelTime = new JLabel("Time: ");
+		labelTime.setBounds(15, 150, 40, 25);
+		
+		textMin = new JTextField("10");
+		textMin.setBounds(60, 150, 30, 25);
+		
+		labelMin = new JLabel("min ");
+		labelMin.setBounds(90, 150, 30, 25);
+
+		textSec = new JTextField("0");
+		textSec.setBounds(120, 150, 30, 25);
+
+		labelSec = new JLabel("sec");
+		labelSec.setBounds(150, 150, 30, 25);
+
+		buttonOk = new JButton("OK");
+		buttonOk.setBounds(80, 185, 80, 25);
+		buttonOk.addActionListener(this);
+
+		buttonCancel = new JButton("Cancel");
+		buttonCancel.setBounds(180, 185, 80, 25);
+		buttonCancel.addActionListener(this);
 		
 		this.add(panelDiff);
 		this.add(checkNode);
+		this.add(comboSolveN);
 		this.add(checkPoint);
+		this.add(comboPauseN);
 		this.add(checkSolver);
 		this.add(checkPause);
 		this.add(checkTime);
+		this.add(labelTime);
+		this.add(textMin);
+		this.add(labelMin);
+		this.add(textSec);
+		this.add(labelSec);
+		this.add(buttonOk);
+		this.add(buttonCancel);
 	}
 	
 	@Override
